a0cef329304921e002cd6eed5528745f493cc49b
Guillermo García <guillermieres97@gmail.com>
2022-10-21 11:49:32 +0200

    feat: list tasks function

--- app/Http/Controllers/Api/TaskController.php
+++ app/Http/Controllers/Api/TaskController.php
@@ -5,8 +5,21 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Task;
+use Error;
+use Throwable;
 
-class ProductController extends Controller
+use function PHPUnit\Framework\throwException;
+
+class TaskController extends Controller
 {
-  
+    public function list()
+    {
+        $tasks = Task::all();
+
+        return response()->json([
+            'status' => 1,
+            'msg' => 'All tasks',
+            'data' => $tasks
+        ], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\TaskController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::namespace('TaskController')->group(function () {
+    Route::get('/tasks/list', [TaskController::class, 'list']);
 });
