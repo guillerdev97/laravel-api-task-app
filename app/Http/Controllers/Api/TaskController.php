7df9b06b3c359473fc51bc1104bacd69b56609e9
Guillermo García <guillermieres97@gmail.com>
2022-10-22 08:13:16 +0200

    feat: create test TaskControllerTest list function case tasks equals to zero

--- app/Http/Controllers/Api/TaskController.php
+++ app/Http/Controllers/Api/TaskController.php
@@ -10,19 +10,25 @@ class TaskController extends Controller
 {
     public function list()
     {
-        $tasks = Task::all();
+        try {
+            $tasks = Task::all();
 
-        if (sizeof($tasks) === 0) {
+            if (sizeof($tasks) === 0) {
+               throw new Exception();
+            }
+    
             return response()->json([
-                'status' => 0,
-                'msg' => 'There are not tasks'
+                'status' => 1,
+                'msg' => 'All tasks',
+                'data' => $tasks
             ], 200);
-        }
+        } catch(Exception $e) {
+            report($e);
 
-        return response()->json([
-            'status' => 1,
-            'msg' => 'All tasks',
-            'data' => $tasks
-        ], 200);
+             return response()->json([
+                'status' => 0,
+                'msg' => 'There are not tasks',
+            ], 404);
+        }
     }
 }
--- tests/Feature/Api/TaskControllerTest.php
+++ tests/Feature/Api/TaskControllerTest.php
@@ -10,19 +10,38 @@ use Tests\TestCase;
 class TaskControllerTest extends TestCase
 {
     use RefreshDatabase;
-    public function test_user_can_see_all_tasks()
+    public function test_user_can_see_all_tasks_if_length_more_than_zero()
     {
         //  given
         Category::factory(3)->create();
         Task::factory(5)->create();
 
         // when
-        $response = $this->get(route('listTasks'));
+        $response = $this->getJson(route('listTasks'));
 
         // then
         $response->assertStatus(200);
+
         $data = $response->decodeResponseJson();
+
         $array = $data['data'];
         $this->assertCount(5, $array);
+    }
+
+    public function test_user_can_see_error_tasks_if_length_equals_to_zero()
+    {
+        //  given
+        Category::factory(3)->create();
+
+        // when
+        $response = $this->getJson(route('listTasks'));
+
+        // then
+        $response->assertStatus(404);
+
+        $data = $response->decodeResponseJson();
+
+        $array = $data['msg'];
+        $this->assertEquals('There are not tasks', $array);
     }
 }
